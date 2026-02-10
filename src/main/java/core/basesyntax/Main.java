55f8cde5c3d1666703b00f6265bba51a637dfcdc
Sergey <shybko2018@gmail.com>
2026-02-10 16:45:43 +0100

    Working-on-taking-tests

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -1,15 +1,9 @@
 package core.basesyntax;
 
 public class Main {
-    private static LocalDate localDate = new LocalDate();
-    private static Main main = new Main();
 
     public static void main(String[] args) {
-        localDate.setAprilDate();
-        localDate.getAprilDate();
-        System.out.println(localDate.toString());
-        String text = String.valueOf(localDate.hours);
-        System.out.println(text);
-        System.out.println(localDate.setAprilDate2());
+        SalaryInfo salaryInfo = new SalaryInfo();
+        System.out.println(salaryInfo.terminalSalaryInfo());
     }
 }
--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,5 +1,8 @@
 package core.basesyntax;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class SalaryInfo {
     private final String[] names = new String[]{"John", "Andrew", "Kate"};
 
@@ -17,14 +20,37 @@ public class SalaryInfo {
     private final String dateForm = "1.04.2019";
     private final String dateTo = "30.04.2019";
 
-    private final int hours = 0;
-    private final int rate = 0;
-    private final int sumSalary = hours * rate;
-
-
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        int index = 0;
+        int salaryJohn = 0;
+        int salaryAndrew = 0;
+        int salaryKate = 0;
 
-        return null;
+        //DateTimeFormatter formatter = DateTimeFormatter.ofPattern("d.MM.yyyy");
+        //LocalDate from = LocalDate.parse(dateFrom, formatter);
+        //LocalDate to = LocalDate.parse(dateTo, formatter);
+
+        for (int i = 0; i < data.length; i++) {
+            String dateMonth = data[i].split(" ")[0];
+            String person = data[i].split(" ")[1];
+            int hours = Integer.parseInt(data[i].split(" ")[2]);
+            int rate = Integer.parseInt(data[i].split(" ")[3]);
+
+            String monthApril = dateMonth.substring(3, 5);
+            if (monthApril.equals("04") && person.equals("John")) {
+                salaryJohn += hours * rate;
+            } else if (monthApril.equals("04") && person.equals("Andrew")) {
+                salaryAndrew += hours * rate;
+            } else if (monthApril.equals("04") && person.equals("Kate")) {
+                salaryKate += hours * rate;
+            }
+        }
+        return "Report for period " + dateForm + " - " + dateTo + " \n"
+                + names[0] + " - " + salaryJohn + " \n"
+                + names[1] + " - " + salaryAndrew + " \n"
+                + names[2] + " - " + salaryKate;
+    }
+
+    public String terminalSalaryInfo() {
+        return getSalaryInfo(names, data, dateForm, dateTo);
     }
 }
